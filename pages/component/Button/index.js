51e5d5fce4d2c1c7839981efdb77ac47099afc47
Ossany Insua Morfa <ossany.insua1982@gmail.com>
2021-09-14 14:47:24 -0400

    other change

---
+++ pages/component/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../../styles/Home.module.css'
+
+const index = ({onClick, children}) => {
+    return (
+        <button className={styles.button} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+export default index;
